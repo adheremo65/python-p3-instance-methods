95c9ae4444c8f2996dff58e6ee87b22975f4704e
Abdela Umer <adheremo65@gmail.com>
2023-07-19 00:15:06 -0400

    python oop introdution

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def bark(self):
+        print("Woof!")
+        pass
+    def sit(self):
+        print("The dog is sitting.")
     # Class body goes here
+fido = Dog()
+fido.bark()
 
     #Instance method definition
-    pass
+    
